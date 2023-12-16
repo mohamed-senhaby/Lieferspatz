4cbd420422e9589df2574d3e9f11420fa218f0a8
mohamed-senhaby <mohamed.wagih.senhaby@gmail.com>
2023-12-16 20:32:05 +0100

    setup server

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import "dotenv/config";
 
-// process.env.MONGO_URL
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the 'public' directory
+app.use(express.static(__dirname + "/public"));
+
+// Define your routes
+app.get("/", (req, res) => {
+  try {
+    res.sendFile(__dirname + "/views/main.html");
+  } catch (error) {
+    console.error("Error retrieving items:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
